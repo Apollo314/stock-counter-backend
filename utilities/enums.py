1e508445c48d16f9bae21ddadd193870720e9ac8
Apollo314 <abidinsefa@outlook.com>
2023-08-10 04:12:35 +0300

    remove unnecessary enum thanks to turkish government

--- utilities/enums.py
+++ utilities/enums.py
@@ -1,30 +1,15 @@
-from decimal import Decimal
-
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Currency(models.TextChoices):
-    turkish_lira = "TRY", "Türk Lirası"
-    dollar = "USD", "US Dollars"
-    euro = "EUR", "Euro"
-    pound = "GBP", "Pound sterling"
-
-
-class KDV(models.IntegerChoices):
-    zero = 0, "%0"
-    one = 1, "%1"
-    eight = 8, "%8"
-    eighteen = 18, "%18"
+    turkish_lira = "TRY", _("Turkish lira")
+    dollar = "USD", _("US dollars")
+    euro = "EUR", _("Euro")
+    pound = "GBP", _("Pound sterling")
 
 
 class InvoiceType(models.TextChoices):
-    purchase = "purchase"
-    sale = "sale"
-    refund_purchase = "refund-purchase"
-    refund_sale = "refund-sale"
-
-
-class StockMovementType(models.TextChoices):
     purchase = "purchase"
     sale = "sale"
     refund_purchase = "refund-purchase"
